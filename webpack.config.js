4d315c3923f270084eba16c1e8a28bec53f15e43
Kimberly Harrod <kim@fios.com>
2017-06-29 13:09:32 -0500

    Webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+	// Entry point of this react application
+	entry: "./app/app.js",
+
+	// Plain compiled javascript is output to this file
+	output: {
+		filename: "public/bundle.js"
+	},
+
+	// Transformations to be performed
+	module: {
+		loaders: [
+			{
+			  // Work with files that have a .js or .jsx extenstion
+			  test: /\.jsx?$/,
+
+			  // Webpack will only process files in the app folder
+			  include: /app/,
+			  loader: "babel",
+			  query: {
+
+			  	// Specific transformations used
+			  	presets: ["react", "es2015"]
+
+			  }
+
+			}
+		]
+	},
+
+	// Allows us to debug our react code in chrome dev tools
+	devtool: "eval-source-map"
+
+};
