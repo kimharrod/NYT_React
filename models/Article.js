77e141a9c41cffd41753942c73b7c99cafe30be0
Kimberly Harrod <kim@fios.com>
2017-06-29 22:59:13 -0500

    Article model

---
+++ models/Article.js
@@ -0,0 +1,27 @@
+// Require Mongoose
+var mongoose = require("mongoose");
+
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+
+	title: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+	},
+	url: {
+		type: String,
+		required: true
+	}
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
